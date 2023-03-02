b5741dd40f04de9232170ce39d5571c6d83547d1
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-03-02 14:20:18 +0530

    create coredata stack - connection with coredata and fetch and save data logic coding apply with entity name

---
+++ MoneyKeeper/Model/CoreDataStack.swift
@@ -0,0 +1,38 @@
+//
+//  CoreDataStack.swift
+//  MoneyKeeper
+//
+//  Created by Bhumika Patel on 02/03/23.
+//
+
+import CoreData
+
+class CoreDataStack {
+    
+    private let containerName: String
+    var viewContext: NSManagedObjectContext { persistentContainer.viewContext }
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                print(error.localizedDescription)
+            }
+            print(storeDescription)
+        })
+        return container
+    }()
+    
+    init(containerName: String) {
+        self.containerName = containerName
+        _ = persistentContainer
+    }
+}
+
+extension NSManagedObjectContext {
+    
+    func saveContext() throws {
+        guard hasChanges else { return }
+        try save()
+    }
+}

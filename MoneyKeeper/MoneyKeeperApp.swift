a9f3a93e28d2578e7bb01d6b50b9cf1eb52a6591
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-03-02 13:49:15 +0530

    Initial Commit

---
+++ MoneyKeeper/MoneyKeeperApp.swift
@@ -0,0 +1,20 @@
+//
+//  MoneyKeeperApp.swift
+//  MoneyKeeper
+//
+//  Created by Bhumika Patel on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MoneyKeeperApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

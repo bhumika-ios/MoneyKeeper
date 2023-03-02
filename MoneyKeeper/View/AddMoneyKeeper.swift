143b715aefe8c4d50d29361f545c0fe2885390f1
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2023-03-02 14:07:09 +0530

    create group mvc architecture - then add money keeper file - define name, date and amount the used navigation view and form declare

---
+++ MoneyKeeper/View/AddMoneyKeeper.swift
@@ -0,0 +1,43 @@
+//
+//  AddMoneyKeeper.swift
+//  MoneyKeeper
+//
+//  Created by Bhumika Patel on 02/03/23.
+//
+
+import SwiftUI
+import CoreData
+
+struct AddMoneyKeeper: View {
+    @State var name: String = ""
+    @State var amount: Double = 0
+    @State var date: Date = Date()
+   
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .disableAutocorrection(true)
+                TextField("Amount", value: $amount)
+                    .keyboardType(.numbersAndPunctuation)
+                    
+          
+                DatePicker(selection: $date, displayedComponents: .date) {
+                    Text("Date")
+                }
+            }
+
+            .navigationBarItems(
+                leading: Button(action: self.onCancelTapped) { Text("Cancel")},
+                trailing: Button(action: self.onSaveTapped) { Text("Save")}
+            ).navigationBarTitle("Add MoneyDetails")
+            
+        }
+    }
+}
+
+struct AddMoneyKeeper_Previews: PreviewProvider {
+    static var previews: some View {
+        AddMoneyKeeper()
+    }
+}
